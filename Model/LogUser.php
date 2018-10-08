2fe86d4b46731d9b696ba5fa4bb7a52dc9673e54
kinderka <43971350+kinderka@users.noreply.github.com>
2018-10-08 22:53:27 +0200

    Add files via upload

---
+++ Model/LogUser.php
@@ -0,0 +1,43 @@
+﻿<?php
+
+    function loginUser()
+    {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        $username = htmlentities($username, ENT_QUOTES, "UTF-8");
+        $password = htmlentities($password, ENT_QUOTES, "UTF-8");
+
+        $database = new Database();
+        $connection = $database -> connect();
+
+        if($query= $database->query(
+        sprintf("SELECT * FROM user WHERE login='%s' AND password='%s'",
+        mysqli_real_escape_string($connection,$username),
+        mysqli_real_escape_string($connection,$password))))
+        {
+            $how_many = $query->num_rows;
+            if($how_many>0)
+            {
+                $_SESSION['logged'] = true;
+
+                $row = $query->fetch_assoc();
+                $_SESSION['id'] = $row['id_user'];
+                $_SESSION['name'] = $row['name'];
+                $_SESSION['surname'] = $row['surname'];
+                $_SESSION['email'] = $row['email'];
+                
+                unset($_SESSION['error']);
+                $query->free_result();
+
+                header('Location: ../View/index-login.php');
+
+            }else{
+
+                $_SESSION['error'] = '<span style="color:red">Nieprawidłowy login lub hasło!</span>';
+                header('Location: ../View/login.php');
+            }
+        }
+    }
+	
+?>
---
+++ Model/Subject.php
@@ -0,0 +1,50 @@
+<?php
+
+    function insertAd()
+    {
+        session_start();
+        
+        $temp_name = $_POST["Przedmiot"];
+        $temp_level = $_POST["Poziom"];
+        $temp_ad = $_POST["Opis"];
+        $temp_place = $_POST["Miejsce"];
+        $temp_pay = $_POST["Wynagrodzenie"];
+
+        $addAD = true;
+
+        $database = new Database();
+        $db = $database->connect();
+
+        if($temp_name && $temp_level && $temp_ad && $temp_place )
+        {
+            $addAD==true;
+
+            if(ctype_digit($temp_pay)==false)
+            {
+                $addAD=false;
+                $_SESSION['e_pay']="Pole musi zawierać tylko cyfry!";
+                header("Location: ../View/advert.php");
+            }
+
+            if($addAD==true)
+            {
+                if($db->query("INSERT INTO `subject` ( `subject`, `level`, `ad`, `place_of_lesson`, `pay`) 
+                VALUES ('$temp_name', '$temp_level', '$temp_ad', '$temp_place', '$temp_pay')"))
+                {
+                    $id_subject = $db->insert_id;
+                    $id_user = $_SESSION['id_user'];
+                    $database->query("UPDATE `subject` SET `user_id_user`='$id_user' WHERE `subject`.`id_subject` = '$id_subject'");
+               
+                    header('Location: ../View/profile.php?msg=OK');
+                } 
+                else
+                echo "Nie dodano nowego ogłoszenia";
+            }
+            else
+            {
+                echo "Uzupełnij dane.";
+                die();
+            }
+        }
+    }
+?>
