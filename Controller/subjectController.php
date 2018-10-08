d5de46b5742fc0e07e18cd1b572e24b5451e82c0
kinderka <43971350+kinderka@users.noreply.github.com>
2018-10-08 22:50:43 +0200

    Update subjectController.php

--- Controller/subjectController.php
+++ Controller/subjectController.php
@@ -1,17 +1,19 @@
 <?php
+
 include_once '../Config/database.php';
 include_once '../Model/Subject.php';
 
-    if(isset($_GET['method'])){
-        if($_GET['method']=="insertAd"){
-            insertAd();
-        }
-        else{
-            echo "Wrong metod name!";
-            die();
-        }
-    }else{
+if(isset($_GET['method'])){
+    if($_GET['method']=="insertAd"){
+        insertAd();
+    }
+    else{
         echo "Wrong metod name!";
         die();
+    }
+}else{
+    echo "Wrong metod name!";
+    die();
 }
+
 ?>
